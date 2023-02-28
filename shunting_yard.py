14fbbb3c779b4c0de6ddaf667b5ac9f700541484
Paul Kern <pkern@laas.fr>
2023-02-28 11:51:11 +0100

    Tokenize done

--- shunting_yard.py
+++ shunting_yard.py
@@ -1,10 +1,19 @@
+from enum import Enum
 from string import ascii_lowercase
+from typing import Iterator
 
 BASE_OPERATORS = '+-*/^'
 NUMBER_CHARS = '0123456789.'
 LETTERS_CHARS = ascii_lowercase + '_'
 
-def tokenize(string: str):
+class TokenType(Enum):
+    BRACKET = 0
+    INTEGER = 10
+    FLOAT = 11
+    OPERATOR = 20
+    FUNCTION = 30
+
+def tokenize(string: str) -> Iterator[tuple[TokenType, str]]:
     if string == '':
         return
 
@@ -12,17 +21,19 @@ def tokenize(string: str):
     cursor = 0
     while cursor < len(string):
         char = string[cursor]
-        if char in BASE_OPERATORS or char in '()':
-            yield char
+        if char in BASE_OPERATORS:
+            yield (TokenType.OPERATOR, char)
+            cursor += 1
+        elif char in '()':
+            yield (TokenType.BRACKET, char)
             cursor += 1
         elif char in NUMBER_CHARS:
             # Go through until not a number anymore
             cursor_end = cursor + 1
             while cursor_end < len(string) and string[cursor_end] in NUMBER_CHARS:
                 cursor_end += 1
-            # Convert either to float or int
-            number = string[cursor:cursor_end]
-            yield float(number) if '.' in number else int(number)
+
+            yield (TokenType.FLOAT if '.' in string[cursor:cursor_end] else TokenType.INTEGER, string[cursor:cursor_end])
             cursor += (cursor_end - cursor)
         elif char in LETTERS_CHARS:
             # Go through until not a number anymore
@@ -30,10 +41,8 @@ def tokenize(string: str):
             while  cursor_end < len(string) and string[cursor_end] in LETTERS_CHARS:
                 cursor_end += 1
 
-            yield string[cursor:cursor_end]
+            yield (TokenType.FUNCTION, string[cursor:cursor_end])
             cursor += (cursor_end - cursor)
         else:
             cursor += 1
 
-
-print(list(tokenize('1+3')))
--- test_shunting_yard.py
+++ test_shunting_yard.py
@@ -1,52 +1,51 @@
 import unittest
 
-from shunting_yard import tokenize
+from shunting_yard import tokenize, TokenType as TT
 
 class TestTokenizer(unittest.TestCase):
 
     def test_base_operations(self):
-        self.assertListEqual(list(tokenize('1+2')), [1, '+', 2])
-        self.assertListEqual(list(tokenize('1-2')), [1, '-', 2])
-        self.assertListEqual(list(tokenize('1*2')), [1, '*', 2])
-        self.assertListEqual(list(tokenize('1/2')), [1, '/', 2])
-        self.assertListEqual(list(tokenize('1^2')), [1, '^', 2])
+        self.assertListEqual(list(tokenize('1+2')), [(TT.INTEGER, '1'), (TT.OPERATOR, '+'), (TT.INTEGER, '2')])
+        self.assertListEqual(list(tokenize('1-2')), [(TT.INTEGER, '1'), (TT.OPERATOR, '-'), (TT.INTEGER, '2')])
+        self.assertListEqual(list(tokenize('1*2')), [(TT.INTEGER, '1'), (TT.OPERATOR, '*'), (TT.INTEGER, '2')])
+        self.assertListEqual(list(tokenize('1/2')), [(TT.INTEGER, '1'), (TT.OPERATOR, '/'), (TT.INTEGER, '2')])
+        self.assertListEqual(list(tokenize('1^2')), [(TT.INTEGER, '1'), (TT.OPERATOR, '^'), (TT.INTEGER, '2')])
 
     def test_spaces(self):
-        self.assertListEqual(list(tokenize('1    + 2             - 3')), [1, '+', 2, '-', 3])
+        self.assertListEqual(list(tokenize('1    + 2             - 3')), [(TT.INTEGER, '1'), (TT.OPERATOR, '+'), (TT.INTEGER, '2'), (TT.OPERATOR, '-'), (TT.INTEGER, '3')])
 
     def test_incorrect(self):
-        self.assertListEqual(list(tokenize('1+')), [1, '+'])
+        self.assertListEqual(list(tokenize('1+')), [(TT.INTEGER, '1'), (TT.OPERATOR, '+')])
 
     def test_empty(self):
         self.assertListEqual(list(tokenize('')), [])
 
     def test_brackets(self):
-        self.assertListEqual(list(tokenize('1+(2 * 3) - 4 * (2 / 3)')), [1, '+', '(', 2, '*', 3, ')', '-', 4, '*', '(', 2, '/', 3, ')'])
+        self.assertListEqual(list(tokenize('1+(2 * 3) - 4 * (2 / 3)')), [(TT.INTEGER, '1'), (TT.OPERATOR, '+'), (TT.BRACKET, '('), (TT.INTEGER, '2'), (TT.OPERATOR, '*'), (TT.INTEGER, '3'), (TT.BRACKET, ')'), (TT.OPERATOR, '-'), (TT.INTEGER, '4'), (TT.OPERATOR, '*'), (TT.BRACKET, '('), (TT.INTEGER, '2'), (TT.OPERATOR, '/'), (TT.INTEGER, '3'), (TT.BRACKET, ')')])
 
     def test_multiple_digits_numbers(self):
-        self.assertListEqual(list(tokenize('123456 + 321654 * 3141515 - 2')), [123456, '+', 321654, '*', 3141515, '-', 2])
+        self.assertListEqual(list(tokenize('123456 + 321654 * 3141515 - 2')), [(TT.INTEGER, '123456'), (TT.OPERATOR, '+'), (TT.INTEGER, '321654'), (TT.OPERATOR, '*'), (TT.INTEGER, '3141515'), (TT.OPERATOR, '-'), (TT.INTEGER, '2')])
 
     def test_decimal_numbers(self):
-        self.assertListEqual(list(tokenize('1.23 * 2.354')), [1.23, '*', 2.354])
+        self.assertListEqual(list(tokenize('1.23 * 2.354')), [(TT.FLOAT, '1.23'), (TT.OPERATOR, '*'), (TT.FLOAT, '2.354')])
 
     def test_decimal_numbers_no_leading(self):
-        self.assertListEqual(list(tokenize('.1 + .2')), [0.1, '+', 0.2])
+        self.assertListEqual(list(tokenize('.1 + .2')), [(TT.FLOAT, '.1'), (TT.OPERATOR, '+'), (TT.FLOAT, '.2')])
 
     def test_function_one_argument(self):
-        self.assertListEqual(list(tokenize('sin(1 + 4)')), ['sin', '(', 1, '+', 4, ')'])
+        self.assertListEqual(list(tokenize('sin(1 + 4)')), [(TT.FUNCTION, 'sin'), (TT.BRACKET, '('), (TT.INTEGER, '1'), (TT.OPERATOR, '+'), (TT.INTEGER, '4'), (TT.BRACKET, ')')])
 
     def test_function_no_argument(self):
-        self.assertListEqual(list(tokenize('-3/2*pi')), ['-', 3, '/', 2, '*', 'pi'])
+        self.assertListEqual(list(tokenize('-3/2*pi')), [(TT.OPERATOR, '-'), (TT.INTEGER, '3'), (TT.OPERATOR, '/'), (TT.INTEGER, '2'), (TT.OPERATOR, '*'), (TT.FUNCTION, 'pi')])
 
     def test_function_2_arguments(self):
-        self.assertListEqual(list(tokenize('max(1, 4)')), ['max', '(', 1, 4, ')'])
+        self.assertListEqual(list(tokenize('max(1, 4)')), [(TT.FUNCTION, 'max'), (TT.BRACKET, '('), (TT.INTEGER, '1'), (TT.INTEGER, '4'), (TT.BRACKET, ')')])
 
     def test_function_upper_case(self):
-        self.assertListEqual(list(tokenize('MIN(1;4)')), ['min', '(', 1, 4, ')'])
+        self.assertListEqual(list(tokenize('MIN(1;4)')), [(TT.FUNCTION, 'min'), (TT.BRACKET, '('), (TT.INTEGER, '1'), (TT.INTEGER, '4'), (TT.BRACKET, ')')])
 
     def test_function_underscore(self):
-        self.assertListEqual(list(tokenize('arc_cos(0)')), ['arc_cos', '(', 0, ')'])
-
+        self.assertListEqual(list(tokenize('arc_cos(0)')), [(TT.FUNCTION, 'arc_cos'), (TT.BRACKET, '('), (TT.INTEGER, '0'), (TT.BRACKET, ')')])
 
 if __name__ == '__main__':
     unittest.main()
