aa9dd24e3d59c6d331f6d4139e93524d6835545a
Paul Kern <pkern@laas.fr>
2023-02-28 13:22:06 +0100

    Removed TokenType

--- shunting_yard.py
+++ shunting_yard.py
@@ -6,14 +6,7 @@ BASE_OPERATORS = '+-*/^'
 NUMBER_CHARS = '0123456789.'
 LETTERS_CHARS = ascii_lowercase + '_'
 
-class TokenType(Enum):
-    BRACKET = 0
-    INTEGER = 10
-    FLOAT = 11
-    OPERATOR = 20
-    FUNCTION = 30
-
-def tokenize(string: str) -> Iterator[tuple[TokenType, str]]:
+def tokenize(string: str) -> Iterator[str]:
     if string == '':
         return
 
@@ -21,11 +14,8 @@ def tokenize(string: str) -> Iterator[tuple[TokenType, str]]:
     cursor = 0
     while cursor < len(string):
         char = string[cursor]
-        if char in BASE_OPERATORS:
-            yield (TokenType.OPERATOR, char)
-            cursor += 1
-        elif char in '()':
-            yield (TokenType.BRACKET, char)
+        if char in BASE_OPERATORS or char in '()':
+            yield char
             cursor += 1
         elif char in NUMBER_CHARS:
             # Go through until not a number anymore
@@ -33,7 +23,7 @@ def tokenize(string: str) -> Iterator[tuple[TokenType, str]]:
             while cursor_end < len(string) and string[cursor_end] in NUMBER_CHARS:
                 cursor_end += 1
 
-            yield (TokenType.FLOAT if '.' in string[cursor:cursor_end] else TokenType.INTEGER, string[cursor:cursor_end])
+            yield string[cursor:cursor_end]
             cursor += (cursor_end - cursor)
         elif char in LETTERS_CHARS:
             # Go through until not a number anymore
@@ -41,8 +31,7 @@ def tokenize(string: str) -> Iterator[tuple[TokenType, str]]:
             while  cursor_end < len(string) and string[cursor_end] in LETTERS_CHARS:
                 cursor_end += 1
 
-            yield (TokenType.FUNCTION, string[cursor:cursor_end])
+            yield string[cursor:cursor_end]
             cursor += (cursor_end - cursor)
         else:
             cursor += 1
-
