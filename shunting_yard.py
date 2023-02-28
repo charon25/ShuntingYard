de35907d75e8f7071f162f937fcae64d5766f2ac
Paul Kern <pkern@laas.fr>
2023-02-28 13:53:54 +0100

    Added shunting yard function

--- shunting_yard.py
+++ shunting_yard.py
@@ -1,10 +1,41 @@
 from enum import Enum
+import math
 from string import ascii_lowercase
 from typing import Iterator
 
+
 BASE_OPERATORS = '+-*/^'
 NUMBER_CHARS = '0123456789.'
-LETTERS_CHARS = ascii_lowercase + '_'
+FUNCTION_CHARS = ascii_lowercase + '_'
+
+
+OPERATORS_PRECEDENCE: dict[str, int] = {
+    '+': 10,
+    '-': 10,
+    '*': 20,
+    '/': 20,
+    '^': 30
+}
+
+# Returns the precendence if the operator is one of +-*/^, or infinity if it is a function
+def get_precedence(operator: str):
+    return OPERATORS_PRECEDENCE.get(operator, math.inf)
+
+
+class Associativity(Enum):
+    NONE = 0
+    LEFT = 1
+    RIGHT = 2
+
+OPERATORS_ASSOCIATIVITY: dict[str, Associativity] = {
+    '+': Associativity.LEFT,
+    '-': Associativity.LEFT,
+    '*': Associativity.LEFT,
+    '/': Associativity.LEFT,
+    '^': Associativity.RIGHT,
+}
+
+
 
 def tokenize(string: str) -> Iterator[str]:
     if string == '':
@@ -25,10 +56,10 @@ def tokenize(string: str) -> Iterator[str]:
 
             yield string[cursor:cursor_end]
             cursor += (cursor_end - cursor)
-        elif char in LETTERS_CHARS:
+        elif char in FUNCTION_CHARS:
             # Go through until not a number anymore
             cursor_end = cursor + 1
-            while  cursor_end < len(string) and string[cursor_end] in LETTERS_CHARS:
+            while  cursor_end < len(string) and string[cursor_end] in FUNCTION_CHARS:
                 cursor_end += 1
 
             yield string[cursor:cursor_end]
@@ -36,7 +67,57 @@ def tokenize(string: str) -> Iterator[str]:
         else:
             cursor += 1
 
-
+# Reference : https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 def shunting_yard(string: str) -> str:
-    pass
+    output: list[str] = []
+    operator_stack: list[str] = []
+
+    for index, token in enumerate(tokenize(string)):
+        first_char = token[0]
+
+        if first_char in NUMBER_CHARS:
+            output.append(token)
+
+        elif first_char in FUNCTION_CHARS:
+            operator_stack.append(token)
+
+        elif first_char == '(':
+            operator_stack.append(first_char)
+
+        elif first_char == ')':
+            if len(operator_stack) == 0:
+                raise ValueError('Mismatched brackets.')
+
+            while operator_stack[-1] != '(':
+                output.append(operator_stack.pop())
+                if len(operator_stack) == 0:
+                    raise ValueError('Mismatched brackets.')
+
+            operator_stack.pop() # Pop the '(' left over
+
+            # If there is a function left, pop it to the output
+            if len(operator_stack) > 0 and operator_stack[-1] in FUNCTION_CHARS:
+                output.append(operator_stack.pop())
+
+        elif first_char in BASE_OPERATORS:
+            while (len(operator_stack) > 0 and
+                  (operator := operator_stack[-1]) != '(' and 
+                  (get_precedence(operator) > get_precedence(token) or 
+                        (get_precedence(operator) == get_precedence(token) and
+                        OPERATORS_ASSOCIATIVITY[token] == Associativity.LEFT))):
+                
+                output.append(operator_stack.pop())
+            
+            operator_stack.append(token)
+
+        else:
+            raise ValueError(f'Unknown token : {token}')
+
+    # Empty the stack
+    for token in reversed(operator_stack):
+        if token == '(':
+            raise ValueError('Mismatched brackets.')
+        output.append(token)
+
+    return ' '.join(output)
 
--- test_shunting_yard.py
+++ test_shunting_yard.py
@@ -51,35 +51,41 @@ class TestTokenizer(unittest.TestCase):
 class TestShuntingYard(unittest.TestCase):
 
     def test_base_operations(self):
-        self.assertListEqual(shunting_yard('1+2'), '1 2 +')
-        self.assertListEqual(shunting_yard('1-2'), '1 2 -')
-        self.assertListEqual(shunting_yard('1*2'), '1 2 *')
-        self.assertListEqual(shunting_yard('1/2'), '1 2 /')
-        self.assertListEqual(shunting_yard('1^2'), '1 2 ^')
+        self.assertEqual(shunting_yard('1+2'), '1 2 +')
+        self.assertEqual(shunting_yard('1-2'), '1 2 -')
+        self.assertEqual(shunting_yard('1*2'), '1 2 *')
+        self.assertEqual(shunting_yard('1/2'), '1 2 /')
+        self.assertEqual(shunting_yard('1^2'), '1 2 ^')
 
     def test_priority(self):
-        self.assertListEqual(shunting_yard('2 * 1 + 3'), '2 1 * 3 +')
+        self.assertEqual(shunting_yard('2 * 1 + 3'), '2 1 * 3 +')
 
     def test_brackets(self):
-        self.assertListEqual(shunting_yard('2 * (1 + 3)'), '1 3 + 2 *')
+        self.assertEqual(shunting_yard('2 * (1 + 3)'), '2 1 3 + *')
 
     def test_function_no_argument(self):
-        self.assertListEqual(shunting_yard('2 * pi - 1'), '2 pi * 1 -')
+        self.assertEqual(shunting_yard('2 * pi - 1'), '2 pi * 1 -')
 
     def test_function_one_argument(self):
-        self.assertListEqual(shunting_yard('2 * sin(5/2)'), '5 2 / sin 2 *')
+        self.assertEqual(shunting_yard('2 * sin(5/2)'), '2 5 2 / sin *')
 
     def test_function_two_argument(self):
-        self.assertListEqual(shunting_yard('5 * max(2; 6)'), '5 2 max 5 *')
+        self.assertEqual(shunting_yard('5 * max(2; 6)'), '5 2 6 max *')
 
     def test_associativity(self):
-        self.assertListEqual(shunting_yard('5 ^ 4 ^ 3 ^ 2'), '5 4 3 2 ^ ^ ^')
+        self.assertEqual(shunting_yard('5 ^ 4 ^ 3 ^ 2'), '5 4 3 2 ^ ^ ^')
 
     def test_complex_1(self):
-        self.assertListEqual(shunting_yard('3 + 4 × 2 / ( 1 − 5 ) ^ 2 ^ 3'), '3 4 2 × 1 5 − 2 3 ^ ^ / +')
+        self.assertEqual(shunting_yard('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3'), '3 4 2 * 1 5 - 2 3 ^ ^ / +')
 
-    def test_complex_1(self):
-        self.assertListEqual(shunting_yard('sin ( max ( 2, 3 ) / 3 × pi )'), '2 3 max 3 / pi × sin')
+    def test_complex_2(self):
+        self.assertEqual(shunting_yard('sin ( max ( 2, 3 ) / 3 * pi )'), '2 3 max 3 / pi * sin')
+
+    def test_mismatched_brackets(self):
+        with self.assertRaises(ValueError):
+            shunting_yard('sin(1')
+        with self.assertRaises(ValueError):
+            shunting_yard('1 + 2)')
 
 
 if __name__ == '__main__':
